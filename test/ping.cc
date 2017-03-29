94934e27858d4e21b97982fa90cd2fd8a597427e
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-03-29 16:19:27 +0200

    Add ping test

---
+++ test/ping.cc
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------------
+// File: ping.cc
+// Author: Georgios Bitzes - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * quarkdb - a redis-like highly available key-value store              *
+ * Copyright (C) 2016 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include <gtest/gtest.h>
+#include "qclient/QSet.hh"
+#include "qclient/AsyncHandler.hh"
+#include <set>
+#include <list>
+#include <algorithm>
+
+using namespace qclient;
+static std::string sHost = "localhost";
+static int sPort = 6380;
+
+#define SSTR(message) static_cast<std::ostringstream&>(std::ostringstream().flush() << message).str()
+
+//------------------------------------------------------------------------------
+// Simply ping the server
+//------------------------------------------------------------------------------
+TEST(Ping, one)
+{
+  QClient cl{sHost, sPort};
+
+  redisReplyPtr reply = cl.exec("PING", "hello there").get();
+  ASSERT_TRUE(reply != nullptr);
+  ASSERT_EQ(reply->type, REDIS_REPLY_STRING);
+  ASSERT_GT(reply->len, 0);
+
+  ASSERT_EQ(strcmp(reply->str, "hello there"), 0);
+}
+
+TEST(Ping, pipelined_10k) {
+  std::vector<std::future<redisReplyPtr>> responses;
+
+  QClient cl{sHost, sPort};
+
+  for(size_t i = 0; i < 10000; i++) {
+    responses.push_back(cl.exec("PING", SSTR("hello from ping #" << i)));
+  }
+
+  for(size_t i = 0; i < 10000; i++) {
+    redisReplyPtr reply = responses[i].get();
+    ASSERT_TRUE(reply != nullptr);
+    ASSERT_EQ(reply->type, REDIS_REPLY_STRING);
+    ASSERT_EQ(std::string(reply->str, reply->len), SSTR("hello from ping #" << i));
+  }
+}
