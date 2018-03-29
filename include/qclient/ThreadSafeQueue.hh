0cc6d7fc7d1b817ea138860d3ab82bfd64edddb3
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-03-29 19:36:42 +0200

    Implement thread-safe queue to be used in WriterThread
    Looks like there's lock contention on stagedRequests
    in WriterThread: flushing items to the socket,
    receiving acknowledgements, and adding new items all
    acquire the same mutex, which is silly.
    
    ThreadSafeQueue supports poppers and pushers which
    don't block each other, and efficient, almost-lock-free
    scanning of the entire contents, using a linked list
    of memory blocks.
    
    To be used in WriterThread, and maybe a few other
    places.

---
+++ include/qclient/ThreadSafeQueue.hh
@@ -0,0 +1,201 @@
+//------------------------------------------------------------------------------
+// File: ThreadSafeQueue.hh
+// Author: Georgios Bitzes - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * qclient - A simple redis C++ client with support for redirects       *
+ * Copyright (C) 2016 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#ifndef QCLIENT_THREAD_SAFE_QUEUE_H
+#define QCLIENT_THREAD_SAFE_QUEUE_H
+
+#include <vector>
+#include <memory>
+#include <mutex>
+
+namespace qclient {
+
+//------------------------------------------------------------------------------
+// A relatively efficient thread-safe queue, with the following use in mind:
+// - Pushers should not block poppers, and vice versa.
+// - Ability to scan through the queue, starting from the beginning, using a
+//   long lived iterator.
+// - Obtaining the iterator should be infrequent.
+// - Progressing the iterator should be very fast, and lockless.
+//
+// The items are laid out in memory inside a singly-linked list composed of
+// large memory chunks.
+//
+// This class does no error checking that it is being used correctly, and will
+// blow up if:
+// - Popping non-existent items.
+// - Progressing the iterator past the end of the queue. An iterator is always
+//   assumed to be valid.
+//------------------------------------------------------------------------------
+
+template<typename T, size_t BlockSize>
+struct MemoryBlock {
+  std::array<T, BlockSize> contents;
+  std::unique_ptr<MemoryBlock> next;
+};
+
+template<typename T, size_t BlockSize>
+class ThreadSafeQueue {
+public:
+  ThreadSafeQueue() {
+    reset();
+  }
+
+  //----------------------------------------------------------------------------
+  // Reset all contents, and start sequence numbers from 0 again.
+  //----------------------------------------------------------------------------
+  void reset() {
+    frontSequenceNumber = 0;
+    nextSequenceNumber = 0;
+
+    firstBlockNextToPop = 0;
+    lastBlockNextPos = 0;
+
+    root.reset();
+    lastBlock = nullptr;
+
+    //--------------------------------------------------------------------------
+    // Allocate root.
+    //--------------------------------------------------------------------------
+    root = std::unique_ptr<MemoryBlock<T, BlockSize>>(new MemoryBlock<T, BlockSize>());
+    lastBlock = root.get();
+  }
+
+  //----------------------------------------------------------------------------
+  // Constructs an item inside the queue, and returns that item's unique
+  // sequence number.
+  //----------------------------------------------------------------------------
+  template<typename... Args>
+  int64_t emplace_back(Args&&... args) {
+    std::lock_guard<std::mutex> lock(pushMutex);
+
+    new (&lastBlock->contents[lastBlockNextPos]) T(args...);
+    // lastBlock->contents[lastBlockNextPos] = T(args...);
+    lastBlockNextPos++;
+
+    if(lastBlockNextPos == BlockSize) {
+      allocateBlock();
+    }
+
+    return nextSequenceNumber++;
+  }
+
+  //----------------------------------------------------------------------------
+  // Pops an item from the queue, and returns the item's unique sequence number.
+  //----------------------------------------------------------------------------
+  int64_t pop_front() {
+    std::lock_guard<std::mutex> lock(popMutex);
+
+    firstBlockNextToPop++;
+    if(firstBlockNextToPop == BlockSize) {
+      removeRoot();
+    }
+
+    return frontSequenceNumber++;
+  }
+
+  class Iterator {
+  public:
+    Iterator() {}
+
+    Iterator(MemoryBlock<T, BlockSize> *block, size_t pos, int64_t seq)
+    : currentBlock(block), nextPos(pos), sequenceNumber(seq) {}
+
+    T& item() {
+      return currentBlock->contents[nextPos];
+    }
+
+    int64_t seq() const {
+      return sequenceNumber;
+    }
+
+    const T& item() const {
+      return currentBlock->contents[nextPos];
+    }
+
+    void next() {
+      sequenceNumber++;
+      nextPos++;
+      if(nextPos == BlockSize) {
+        nextPos = 0;
+        currentBlock = currentBlock->next.get();
+      }
+    }
+
+  private:
+    MemoryBlock<T, BlockSize> *currentBlock = nullptr;
+    size_t nextPos = -1;
+    int64_t sequenceNumber;
+  };
+
+  Iterator begin() {
+    //--------------------------------------------------------------------------
+    // We don't lock popMutex here, you should synchronize the two externally.
+    // There's no use case where calling begin() is safe if there are concurrent
+    // pops. By not locking here, we'll get a nice warning with TSan if that
+    // happens.
+    //--------------------------------------------------------------------------
+    return Iterator(root.get(), firstBlockNextToPop, frontSequenceNumber);
+  }
+
+  bool empty() {
+    std::lock_guard<std::mutex> lock(pushMutex);
+    std::lock_guard<std::mutex> lock2(popMutex);
+    return root->next.get() == nullptr && nextSequenceNumber == frontSequenceNumber;
+  }
+
+private:
+  //----------------------------------------------------------------------------
+  // Remove the root node, and make its child the root.
+  //----------------------------------------------------------------------------
+  void removeRoot() {
+    std::unique_ptr<MemoryBlock<T, BlockSize>> child = std::move(root->next);
+    root = std::move(child);
+    firstBlockNextToPop = 0;
+  }
+
+  //----------------------------------------------------------------------------
+  // Allocate new block.
+  //----------------------------------------------------------------------------
+  void allocateBlock() {
+    lastBlock->next = std::unique_ptr<MemoryBlock<T, BlockSize>>(new MemoryBlock<T, BlockSize>());
+    lastBlockNextPos = 0;
+    lastBlock = lastBlock->next.get();
+  }
+
+  std::unique_ptr<MemoryBlock<T, BlockSize>> root;
+  MemoryBlock<T, BlockSize> *lastBlock;
+
+  size_t firstBlockNextToPop;
+  size_t lastBlockNextPos;
+
+  int64_t nextSequenceNumber;
+  int64_t frontSequenceNumber;
+
+  std::mutex pushMutex;
+  std::mutex popMutex;
+};
+
+}
+
+#endif
---
+++ test/thread-safe-queue.cc
@@ -0,0 +1,98 @@
+// ----------------------------------------------------------------------
+// File: thread-safe-queue.cc
+// Author: Georgios Bitzes - CERN
+// ----------------------------------------------------------------------
+
+/************************************************************************
+ * qclient - A simple redis C++ client with support for redirects       *
+ * Copyright (C) 2016 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include <gtest/gtest.h>
+#include "qclient/ThreadSafeQueue.hh"
+
+using namespace qclient;
+
+class Coord {
+public:
+  Coord() {}
+  Coord(int a, int b) : x(a), y(b) {}
+  int x = -1;
+  int y = -1;
+};
+
+
+template <class T>
+class Thread_Safe_Queue : public testing::Test {
+protected:
+  T queue;
+};
+
+typedef ::testing::Types<
+  ThreadSafeQueue<Coord, 1>, ThreadSafeQueue<Coord, 2>, ThreadSafeQueue<Coord, 3>,
+  ThreadSafeQueue<Coord, 4>, ThreadSafeQueue<Coord, 5>, ThreadSafeQueue<Coord, 6>,
+  ThreadSafeQueue<Coord, 7>, ThreadSafeQueue<Coord, 8>, ThreadSafeQueue<Coord, 9>,
+  ThreadSafeQueue<Coord, 10>, ThreadSafeQueue<Coord, 11>, ThreadSafeQueue<Coord, 13>,
+  ThreadSafeQueue<Coord, 16>, ThreadSafeQueue<Coord, 20>, ThreadSafeQueue<Coord, 32>,
+  ThreadSafeQueue<Coord, 100>, ThreadSafeQueue<Coord, 128>, ThreadSafeQueue<Coord, 200>,
+  ThreadSafeQueue<Coord, 333>> Implementations;
+
+TYPED_TEST_CASE(Thread_Safe_Queue, Implementations);
+
+TYPED_TEST(Thread_Safe_Queue, BasicSanity) {
+  ASSERT_TRUE(this->queue.empty());
+  ASSERT_EQ(0, this->queue.emplace_back(1, 2));
+  ASSERT_FALSE(this->queue.empty());
+  auto it = this->queue.begin();
+  ASSERT_EQ(it.seq(), 0);
+
+  Coord *coord = &it.item();
+  ASSERT_EQ(coord->x, 1);
+  ASSERT_EQ(coord->y, 2);
+
+  it.next();
+  ASSERT_EQ(it.seq(), 1);
+
+  ASSERT_EQ(0, this->queue.pop_front());
+  ASSERT_TRUE(this->queue.empty());
+
+  ASSERT_EQ(1, this->queue.emplace_back(2, 3));
+  ASSERT_FALSE(this->queue.empty());
+
+  coord = &it.item();
+  ASSERT_EQ(coord->x, 2);
+  ASSERT_EQ(coord->y, 3);
+  it.next();
+  ASSERT_EQ(it.seq(), 2);
+  ASSERT_EQ(1, this->queue.pop_front());
+  ASSERT_TRUE(this->queue.empty());
+
+  for(size_t i = 0; i < 100; i++) {
+    ASSERT_EQ(2+i, this->queue.emplace_back(i*i, i*i+1));
+    ASSERT_FALSE(this->queue.empty());
+  }
+
+  for(size_t i = 0; i < 100; i++) {
+    coord = &it.item();
+    ASSERT_EQ(coord->x, i*i);
+    ASSERT_EQ(coord->y, i*i+1);
+    it.next();
+    ASSERT_EQ(it.seq(), 3+i);
+    ASSERT_EQ(2+i, this->queue.pop_front());
+  }
+
+  ASSERT_TRUE(this->queue.empty());
+}
