04768922955c9a02bcbdd3547bf8d8efe03d8bcf
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-05-22 10:21:16 +0200

    Implement type for owning file descriptors, modelled after std::unique_ptr

---
+++ include/qclient/network/FileDescriptor.hh
@@ -0,0 +1,90 @@
+//------------------------------------------------------------------------------
+// File: FileDescriptor.hh
+// Author: Georgios Bitzes - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * qclient - A simple redis C++ client with support for redirects       *
+ * Copyright (C) 2019 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#ifndef QCLIENT_FILE_DESCRIPTOR_HH
+#define QCLIENT_FILE_DESCRIPTOR_HH
+
+namespace qclient {
+
+//------------------------------------------------------------------------------
+// Move-only type which owns a FileDescriptor, RAII wrapper.
+//------------------------------------------------------------------------------
+class FileDescriptor {
+public:
+  //----------------------------------------------------------------------------
+  // Constructor - this object takes ownership of the specified file descriptor,
+  // and will call ::close on it on destruction.
+  //
+  // FileDescriptor(-1) means it currently owns nothing.
+  //----------------------------------------------------------------------------
+  FileDescriptor(int fd = -1);
+
+  //----------------------------------------------------------------------------
+  // Destructor
+  //----------------------------------------------------------------------------
+  ~FileDescriptor();
+
+  //----------------------------------------------------------------------------
+  // No copying
+  //----------------------------------------------------------------------------
+  FileDescriptor(const FileDescriptor& other) = delete;
+  FileDescriptor& operator=(const FileDescriptor& other) = delete;
+
+  //----------------------------------------------------------------------------
+  // Only allow moving
+  //----------------------------------------------------------------------------
+  FileDescriptor(FileDescriptor&& other);
+  FileDescriptor& operator=(FileDescriptor&& other);
+
+  //----------------------------------------------------------------------------
+  // Reset file descriptor - reset(-1) simply means clearing current ownership.
+  //----------------------------------------------------------------------------
+  void reset(int newfd = -1);
+
+  //----------------------------------------------------------------------------
+  // Get current file descriptor as integer. Never store it, only pass it to
+  // system calls.. Never call close() on it, either.
+  //----------------------------------------------------------------------------
+  int get() const;
+
+  //----------------------------------------------------------------------------
+  // Release current file descriptor - return its value, and relinquish
+  // ownership.
+  //----------------------------------------------------------------------------
+  int release();
+
+  //----------------------------------------------------------------------------
+  // Check if the object is currently managing an fd
+  //----------------------------------------------------------------------------
+  operator bool() const;
+
+
+private:
+  int fd;
+};
+
+}
+
+
+
+#endif
---
+++ src/network/FileDescriptor.cc
@@ -0,0 +1,99 @@
+//------------------------------------------------------------------------------
+// File: FileDescriptor.cc
+// Author: Georgios Bitzes - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * qclient - A simple redis C++ client with support for redirects       *
+ * Copyright (C) 2019 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include "qclient/network/FileDescriptor.hh"
+#include <unistd.h>
+
+namespace qclient {
+
+//------------------------------------------------------------------------------
+// Constructor - this object takes ownership of the specified file descriptor,
+// and will call ::close on it on destruction.
+//
+// FileDescriptor(-1) means it currently owns nothing.
+//------------------------------------------------------------------------------
+FileDescriptor::FileDescriptor(int fd_) : fd(fd_) {}
+
+//------------------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------------------
+FileDescriptor::~FileDescriptor() {
+  reset();
+}
+
+//------------------------------------------------------------------------------
+// Reset file descriptor - reset(-1) simply means clearing current ownership.
+//------------------------------------------------------------------------------
+void FileDescriptor::reset(int newfd) {
+  if(fd >= 0) {
+    ::close(fd);
+    fd = -1;
+  }
+
+  fd = newfd;
+}
+
+//------------------------------------------------------------------------------
+// Only allow moving
+//------------------------------------------------------------------------------
+FileDescriptor::FileDescriptor(FileDescriptor&& other) {
+  reset();
+  fd = other.fd;
+  other.fd = -1;
+}
+
+FileDescriptor& FileDescriptor::operator=(FileDescriptor&& other) {
+  reset();
+  fd = other.fd;
+  other.fd = -1;
+  return *this;
+}
+
+//------------------------------------------------------------------------------
+// Get current file descriptor as integer. Never store it, only pass it to
+// system calls.. Never call close() on it, either.
+//------------------------------------------------------------------------------
+int FileDescriptor::get() const {
+  return fd;
+}
+
+//------------------------------------------------------------------------------
+// Release current file descriptor - return its value, and relinquish
+// ownership.
+//------------------------------------------------------------------------------
+int FileDescriptor::release() {
+  int retval = fd;
+  fd = -1;
+  return retval;
+}
+
+//------------------------------------------------------------------------------
+// Check if the object is currently managing an fd
+//------------------------------------------------------------------------------
+FileDescriptor::operator bool() const {
+  return fd >= 0;
+}
+
+
+}
+
