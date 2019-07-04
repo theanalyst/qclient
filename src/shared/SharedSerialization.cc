4ba6950f9d4a0f7149c3b2511f258aa757890849
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-07-04 10:31:32 +0200

    Support be64toh and friends on MacOS

--- src/shared/SharedSerialization.cc
+++ src/shared/SharedSerialization.cc
@@ -28,6 +28,27 @@
 #include <string.h>
 #include <iostream>
 
+#ifdef __APPLE__
+
+#include <libkern/OSByteOrder.h>
+
+#define htobe16(x) OSSwapHostToBigInt16(x)
+#define htole16(x) OSSwapHostToLittleInt16(x)
+#define be16toh(x) OSSwapBigToHostInt16(x)
+#define le16toh(x) OSSwapLittleToHostInt16(x)
+
+#define htobe32(x) OSSwapHostToBigInt32(x)
+#define htole32(x) OSSwapHostToLittleInt32(x)
+#define be32toh(x) OSSwapBigToHostInt32(x)
+#define le32toh(x) OSSwapLittleToHostInt32(x)
+
+#define htobe64(x) OSSwapHostToBigInt64(x)
+#define htole64(x) OSSwapHostToLittleInt64(x)
+#define be64toh(x) OSSwapBigToHostInt64(x)
+#define le64toh(x) OSSwapLittleToHostInt64(x)
+
+#endif
+
 static int64_t binaryStringToInt(const char* buff) {
   int64_t result;
   memcpy(&result, buff, sizeof(result));
