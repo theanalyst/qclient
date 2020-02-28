31726cf4381af9e90c8defbae6dfb3296aa99d00
Georgios Bitzes <georgios.bitzes@cern.ch>
2020-02-28 11:52:11 +0100

    Implement BinarySerializer to ease serialization of certain messages

---
+++ src/shared/BinarySerializer.cc
@@ -0,0 +1,110 @@
+//------------------------------------------------------------------------------
+// File: BinarySerializer.cc
+// Author: Georgios Bitzes - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * qclient - A simple redis C++ client with support for redirects       *
+ * Copyright (C) 2020 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include "BinarySerializer.hh"
+#include <string.h>
+
+#ifdef __APPLE__
+
+#include <libkern/OSByteOrder.h>
+
+#define htobe16(x) OSSwapHostToBigInt16(x)
+#define htole16(x) OSSwapHostToLittleInt16(x)
+#define be16toh(x) OSSwapBigToHostInt16(x)
+#define le16toh(x) OSSwapLittleToHostInt16(x)
+
+#define htobe32(x) OSSwapHostToBigInt32(x)
+#define htole32(x) OSSwapHostToLittleInt32(x)
+#define be32toh(x) OSSwapBigToHostInt32(x)
+#define le32toh(x) OSSwapLittleToHostInt32(x)
+
+#define htobe64(x) OSSwapHostToBigInt64(x)
+#define htole64(x) OSSwapHostToLittleInt64(x)
+#define be64toh(x) OSSwapBigToHostInt64(x)
+#define le64toh(x) OSSwapLittleToHostInt64(x)
+
+#endif
+
+namespace qclient {
+
+static void intToBinaryString(int64_t num, char* buff) {
+  int64_t be = htobe64(num);
+  memcpy(buff, &be, sizeof(be));
+}
+
+//------------------------------------------------------------------------------
+//! Constructor
+//------------------------------------------------------------------------------
+BinarySerializer::BinarySerializer(std::string &trg, size_t size)
+: target(trg) {
+
+  target.resize(size);
+  currentPosition = 0;
+}
+
+//------------------------------------------------------------------------------
+// Append int64_t
+//------------------------------------------------------------------------------
+void BinarySerializer::appendInt64(int64_t num) {
+  intToBinaryString(num, target.data() + currentPosition);
+  currentPosition += sizeof(int64_t);
+}
+
+//------------------------------------------------------------------------------
+// Append raw bytes
+//------------------------------------------------------------------------------
+void BinarySerializer::appendBytes(const char* source, size_t len) {
+  memcpy(target.data() + currentPosition, source, len);
+  currentPosition += len;
+}
+
+//------------------------------------------------------------------------------
+// Append string, including the length
+//------------------------------------------------------------------------------
+void BinarySerializer::appendStringWithLength(const std::string &str) {
+  appendInt64(str.size());
+  appendBytes(str.data(), str.size());
+}
+
+//------------------------------------------------------------------------------
+// Get size remaining
+//------------------------------------------------------------------------------
+int64_t BinarySerializer::getRemaining() const {
+  return target.size() - currentPosition;
+}
+
+//------------------------------------------------------------------------------
+//! Constructor
+//------------------------------------------------------------------------------
+BinaryDeserializer::BinaryDeserializer(const std::string &src)
+: source(src), currentPosition(0) {}
+
+//----------------------------------------------------------------------------
+//! Check if there's enough space to extract N bytes
+//----------------------------------------------------------------------------
+bool BinaryDeserializer::canExtract(size_t b) const {
+  return (source.size() - currentPosition) > b;
+}
+
+}
+
---
+++ src/shared/BinarySerializer.hh
@@ -0,0 +1,96 @@
+//------------------------------------------------------------------------------
+// File: BinarySerializer.hh
+// Author: Georgios Bitzes - CERN
+//------------------------------------------------------------------------------
+
+/************************************************************************
+ * qclient - A simple redis C++ client with support for redirects       *
+ * Copyright (C) 2020 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#ifndef QCLIENT_SHARED_BINARY_SERIALIZER_HH
+#define QCLIENT_SHARED_BINARY_SERIALIZER_HH
+
+#include <map>
+#include <string>
+
+namespace qclient {
+
+//------------------------------------------------------------------------------
+//! Helper class for efficiently serializing messages
+//------------------------------------------------------------------------------
+class BinarySerializer {
+public:
+  //----------------------------------------------------------------------------
+  //! Constructor
+  //----------------------------------------------------------------------------
+  BinarySerializer(std::string &target, size_t size);
+
+  //----------------------------------------------------------------------------
+  //! Append int64_t
+  //----------------------------------------------------------------------------
+  void appendInt64(int64_t num);
+
+  //----------------------------------------------------------------------------
+  //! Append raw bytes
+  //----------------------------------------------------------------------------
+  void appendBytes(const char* source, size_t len);
+
+  //----------------------------------------------------------------------------
+  //! Append string, including the length
+  //----------------------------------------------------------------------------
+  void appendStringWithLength(const std::string &str);
+
+  //----------------------------------------------------------------------------
+  //! Get size remaining
+  //----------------------------------------------------------------------------
+  int64_t getRemaining() const;
+
+private:
+  std::string &target;
+  size_t currentPosition;
+
+};
+
+//------------------------------------------------------------------------------
+//! Helper class for efficiently de-serializing messages
+//------------------------------------------------------------------------------
+class BinaryDeserializer {
+public:
+  //----------------------------------------------------------------------------
+  //! Constructor
+  //----------------------------------------------------------------------------
+  BinaryDeserializer(const std::string &source);
+
+
+
+private:
+  //----------------------------------------------------------------------------
+  //! Check if there's enough space to extract N bytes
+  //----------------------------------------------------------------------------
+  bool canExtract(size_t b) const;
+
+
+
+
+  const std::string &source;
+  size_t currentPosition;
+
+};
+
+}
+
+#endif
