eac288274f2c283f62d328542ff36319711e8544
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2024-10-24 17:26:24 +0200

    QCLIENT: Introduce an acktracker for tracking notifications
    This helps for peristency layer for bookkeeping out of order notifications.
    
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

---
+++ include/qclient/utils/AckTracker.hh
@@ -0,0 +1,132 @@
+// File: AckTracker.hh
+// Author: Abhishek Lekshmanan - CERN
+// ----------------------------------------------------------------------
+
+/************************************************************************
+  * qclient - A simple redis C++ client with support for redirects       *
+  * Copyright (C) 2024 CERN/Switzerland                                  *
+  *                                                                      *
+  * This program is free software: you can redistribute it and/or modify *
+  * it under the terms of the GNU General Public License as published by *
+  * the Free Software Foundation, either version 3 of the License, or    *
+  * (at your option) any later version.                                  *
+  *                                                                      *
+  * This program is distributed in the hope that it will be useful,      *
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+  * GNU General Public License for more details.                         *
+  *                                                                      *
+  * You should have received a copy of the GNU General Public License    *
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+  ************************************************************************/
+
+
+#ifndef QCLIENT_ACKTRACKER_HH
+#define QCLIENT_ACKTRACKER_HH
+
+#include <atomic>
+#include <set>
+
+namespace qclient {
+
+using ItemIndex = int64_t;
+
+struct AckTracker {
+  virtual void ackIndex(ItemIndex index) = 0;
+  virtual bool isAcked(ItemIndex index) = 0;
+  virtual ItemIndex getStartingIndex() = 0;
+  virtual ItemIndex getHighestAckedIndex() = 0;
+  virtual ~AckTracker() = default;
+};
+
+class HighestAckTracker : public AckTracker {
+public:
+  HighestAckTracker() = default;
+  ~HighestAckTracker() override = default;
+
+  void
+  ackIndex(ItemIndex index) override
+  {
+    ItemIndex curr_high = nextIndex.load(std::memory_order_acquire);
+    nextIndex.store(std::max(index + 1, curr_high), std::memory_order_release);
+  }
+
+  bool
+  isAcked(ItemIndex index) override
+  {
+    return index < nextIndex.load(std::memory_order_acquire);
+  }
+
+  ItemIndex
+  getStartingIndex() override
+  {
+    return nextIndex.load(std::memory_order_acquire);
+  }
+
+  ItemIndex
+  getHighestAckedIndex() override
+  {
+    return nextIndex.load(std::memory_order_acquire);
+  }
+
+private:
+  std::atomic<ItemIndex> nextIndex{0};
+};
+
+class SetAckTracker : public AckTracker {
+
+public:
+  SetAckTracker() = default;
+  ~SetAckTracker() override = default;
+
+  void
+  ackIndex(ItemIndex index) override
+  {
+    std::scoped_lock wr_lock(mtx);
+    acked.insert(index);
+    updateStartingIndex();
+  }
+
+  bool
+  isAcked(ItemIndex index) override
+  {
+    std::scoped_lock rd_lock(mtx);
+    if (index < startingIndex) {
+      return true;
+    }
+    return acked.find(index) != acked.end();
+  }
+
+  ItemIndex
+  getStartingIndex() override
+  {
+    return startingIndex;
+  }
+
+  ItemIndex
+  getHighestAckedIndex() override
+  {
+    std::scoped_lock rd_lock(mtx);
+    if (acked.empty()) {
+      return 0;
+    }
+    return *acked.rbegin();
+  }
+
+private:
+  void
+  updateStartingIndex()
+  {
+    while (!acked.empty() && *acked.begin() == startingIndex) {
+      acked.erase(acked.begin());
+      ++startingIndex;
+    }
+  }
+  std::set<ItemIndex> acked;
+  std::mutex mtx;
+  ItemIndex startingIndex{0};
+};
+
+} // namespace qclient
+
+#endif // EOS_ACKTRACKER_HH
