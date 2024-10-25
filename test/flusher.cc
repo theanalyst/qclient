f4811fe8d4763de6068afd95eb7e10321e8761ab
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2024-10-25 16:02:22 +0200

    TEST: add tests for using RocksDB and set based acktracker
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- test/flusher.cc
+++ test/flusher.cc
@@ -174,11 +174,12 @@ TEST_F(QDBFlusherInstance, RocksDBPersistencyMultiPushLockfree)
 {
   auto opts = getQCOpts();
   opts.backpressureStrategy = BackpressureStrategy::RateLimitPendingRequests(1ULL<<22);
+  std::string rocks_options;
   qclient::BackgroundFlusher flusher(members, std::move(opts),
                                      dummyNotifier,
-                                     std::make_unique<qclient::ParallelRocksDBPersistency>(tmp_dir),
+                                     std::make_unique<qclient::ParallelRocksDBPersistency>(tmp_dir, rocks_options, std::make_unique<qclient::SetAckTracker>()),
                                      qclient::FlusherQueueHandlerT::LockFree);
-  testMultiPush(flusher, "rocksdb_lockfree");
+  testMultiPush(flusher, "rocksdb_lockfree:set");
 }
 
 TEST_F(QDBFlusherInstance, RocksDBMultiPushBuilder)
